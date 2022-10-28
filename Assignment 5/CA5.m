2b5da4b348d31e3e5bd6c961387815c079b01fd3
Daniel <dgt1@sfu.ca>
2022-10-28 14:57:38 -0700

    Still sorting out issue with calling barryinterp

--- Assignment 5/CA5.m
+++ Assignment 5/CA5.m
@@ -8,13 +8,15 @@ f2 = @(x) 1/(1+16*(x^2));
 for n=3:nMax
     %Compute the equally-spaced nodes
     for i=0:n
-        nodes(i+1) = -1+(2*i)/n;
+        x(i+1) = -1+(2*i)/n;
     end
+    %Compute grid?
+    grid = -1: 2/(n-1) : 1;
     %Compute the interpolating polynomial for f1(x) and f2(x)
-    w1 = baryweights(nodes);
-    w2 = baryweights(nodes);
-    u1 = baryinterp(nodes, w1, f1, nodes);
-    u2 = baryinterp(nodes, w2, f2, nodes);
+    w1 = baryweights(x);
+    w2 = baryweights(x);
+    u1 = baryinterp(x, w1, f1, grid); %<-- something is wrong here
+    u2 = baryinterp(x, w2, f2, grid);
     %Compute the error of the interpolating polynomial
     eVec1 = [];
     eVec2 = [];
