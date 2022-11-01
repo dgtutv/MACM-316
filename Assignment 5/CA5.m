5038621bdf306fd8192db68c6ae5f48bd6d3bcdd
Daniel <dgt1@sfu.ca>
2022-10-31 17:27:12 -0700

    Working on bugs, does not work

--- Assignment 5/CA5.m
+++ Assignment 5/CA5.m
@@ -2,38 +2,46 @@
 nodes = [];
 en1 = [];
 en2 = [];
-nMax = 20;
-x = [];
-f1 = @(x) 1/(5-4*x);
-f2 = @(x) 1/(1+16*(x^2));
-for n=3:nMax
+n = [5; 10; 50; 100; 200; 500; 1000];
+%Choose the values we wish to interpolate at
+interpX = [-.99, -.73 -.62, -.43 -.25, .12, .31, .49, .59, .86];
+for i=1:length(n)
     %Compute the equally-spaced nodes
-    for i=0:n-1
-        x(i+1) = -1+(2*i)/(n-1);
+    xi = -1:2/n(i, 1):1;
+    xi=xi';
+    %Compute the barryweights
+    bw = baryweights(xi);  
+    %Calculate the y values of the function at each x in xi
+    for j=1:length(xi)
+        y1(j,1) = f1(xi(j,1));
+        y2(j,1) = f2(xi(j,1));
     end
-    %Compute grid?
-    disp(f1(x)) %Compute for each x, then make grid
-    grid1 = meshgrid(x, f1(x));
-    grid2 = meshgrid(x, f2(x));
-    %Compute the interpolating polynomial for f1(x) and f2(x)
-    w1 = baryweights(x);
-    w2 = baryweights(x);
-    disp(w1)
-    disp(w2)
-    u1 = baryinterp(x, w1, f1, grid1); %<-- something is wrong here
-    u2 = baryinterp(x, w2, f2, grid2);
+    %bary interp
+    u1 = baryinterp(xi, bw, y1, interpX);
+    u2 = baryinterp(xi, bw, y2, interpX);
     %Compute the error of the interpolating polynomial
     eVec1 = [];
     eVec2 = [];
     for x=-1: 2/(n-1) :1
-        eVec1 = abs(u1(x) - f1(x));
-        eVec2 = abs(u2(x) - f2(x));
+        eVec1(x) = abs(u1(x) - f1(x));
+        eVec2(x) = abs(u2(x) - f2(x));
     end
-    en1(n) = max(eVec1);
-    en2(n) = max(eVec2);
+    disp(eVec1)
+    disp(eVec2)
+    en1(i) = max(eVec1);
+    en2(i) = max(eVec2);
 end
+    
 %Plot log10(en) vs n
 x = linspace(3, nMax);
 plot(x, en1);
 figure;
 plot(x, en2);
+
+%Functions
+function y = f1(x)
+    y=1/(5-4*x);
+end
+function y = f2(x)
+    y=1/(1+16*(x^2));
+end
--- Assignment 5/baryweights.m
+++ Assignment 5/baryweights.m
@@ -1,9 +1,11 @@
 function w = baryweights(x)
-    n=length(x);
-    w=zeros(n,1);
-    for i=1:n
-        X=x-x(i);
-        X=X([1:i-1 i+1:n]);
-        w(i)=1/prod(X);
-    end
+% 
+% Description
+
+n=length(x);
+w=zeros(n,1);
+for i=1:n 
+    X=x-x(i)*ones(n,1);
+    X=X([1:i-1 i+1:n],1);
+    w(i)=1/prod(X);
 end
