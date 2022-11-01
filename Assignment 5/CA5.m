bc1872da5e510845620fb7ccdc7d820279e690f8
Daniel <dgt1@sfu.ca>
2022-10-31 17:32:33 -0700

    Needed to reset variables each iteration

--- Assignment 5/CA5.m
+++ Assignment 5/CA5.m
@@ -3,6 +3,7 @@ nodes = [];
 en1 = [];
 en2 = [];
 n = [5; 10; 50; 100; 200; 500; 1000];
+xi = [];
 %Choose the values we wish to interpolate at
 interpX = [-.99, -.73 -.62, -.43 -.25, .12, .31, .49, .59, .86];
 for i=1:length(n)
@@ -12,6 +13,8 @@ for i=1:length(n)
     %Compute the barryweights
     bw = baryweights(xi);  
     %Calculate the y values of the function at each x in xi
+    y1 = [];
+    y2 = [];
     for j=1:length(xi)
         y1(j,1) = f1(xi(j,1));
         y2(j,1) = f2(xi(j,1));
@@ -23,13 +26,11 @@ for i=1:length(n)
     eVec1 = [];
     eVec2 = [];
     for x=-1: 2/(n-1) :1
-        eVec1(x) = abs(u1(x) - f1(x));
-        eVec2(x) = abs(u2(x) - f2(x));
+        eVec1 = abs(u1(x) - f1(x));
+        eVec2 = abs(u2(x) - f2(x));
     end
-    disp(eVec1)
-    disp(eVec2)
-    en1(i) = max(eVec1);
-    en2(i) = max(eVec2);
+    en1(n) = max(eVec1);
+    en2(n) = max(eVec2);
 end
     
 %Plot log10(en) vs n
