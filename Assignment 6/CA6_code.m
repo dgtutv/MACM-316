a45d47f8d4f8cd840b3256c88250222b37ce7ddc
Daniel <dgt1@sfu.ca>
2022-11-02 22:01:08 -0700

    Finished some more inital work on the assignment as I read it

--- Assignment 6/CA6_code.m
+++ Assignment 6/CA6_code.m
@@ -3,3 +3,14 @@ for i=1:n
     b=fzero(@(x) x*exp(x)-i*pi,0);
     a(i)=exp(-b);
 end
+Q = zeros();
+for i=1:length(a)
+    %Compute integral woth numerical quadrature
+    I = integral(f(x), a(i), x(i+1));
+    %Compute Q(n) sum
+    Q(n) = Q(n) + I;
+end
+
+function y = f(x)
+    y = (x^-1)*sin((x^-1)*log(x));
+end
