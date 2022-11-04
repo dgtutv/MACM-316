4c17393da5cd8f0f5435d5b82ffe9e4e43cfc70e
Daniel <dgt1@sfu.ca>
2022-11-03 22:42:53 -0700

    Believe that the main part of assignment complete, need to find optimal N

--- Assignment 6/CA6_code.m
+++ Assignment 6/CA6_code.m
@@ -15,4 +15,27 @@ for n=100:100:1000
 end
 format longg
 %How many digits of I can you accurately compute?
-disp(Q)
+%disp(Q)
+%Let q be Q hat
+q=[];
+N=2000;
+for n=100:100:N
+    Q(n/100) = 0;
+    for i=1:n
+        b=fzero(@(x) x*exp(x)-i*pi,0);
+        a(i)=exp(-b);
+        if i>1
+            %Compute integral with numerical quadrature
+            I = integral(f, a(i-1), a(i));
+            %Compute Q(n) sum
+            Q(n/100) = Q(n/100) + I; 
+        end
+    end
+    if (n/100)>3
+        %Compute Aitken's triangle square method
+        q((n/100)-3) = Q((n/100)-3) - (( Q((n/100)-2) - Q((n/100)-1) ).^2 / ( Q(n/100) - 2.*Q((n/100)-1) + Q((n/100)-2) ));
+    end
+end
+for i=length(q)-5:length(q)
+    disp(q(i));
+end
