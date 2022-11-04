3759f5d2e888146196832f2708c644f7f1403f38
Daniel <dgt1@sfu.ca>
2022-11-04 10:50:06 -0700

    commit before working on laptop

--- Assignment 6/CA6_code.m
+++ Assignment 6/CA6_code.m
@@ -5,7 +5,7 @@ for n=100:100:1000
     for i=1:n
         b=fzero(@(x) x*exp(x)-i*pi,0);
         a(i)=exp(-b);
-        if i~=1
+        if i>1
             %Compute integral with numerical quadrature
             I = integral(f, a(i-1), a(i));
             %Compute Q(n) sum
@@ -21,7 +21,10 @@ for i=length(Q)-5:length(Q)
     disp(Q(i));
 end
 
-%Let q be Q hat
+% b_func = @(x) x*exp(x)-i*pi;
+% c=1;
+% N=7000;
+
 q=[];
 N=7000;
 for n=100:100:N
@@ -47,4 +50,26 @@ disp("Last 5 Q hat:")
 for i=length(q)-5:length(q)
     format longg
     disp(q(i));
+end
+
+function q = computeIntegral(b_func, c, f, N)
+    %Let q be Q hat
+    q=[];
+    for n=100:100:N
+        Q(n/100) = 0;
+        for i=1:n
+            b=fzero(b_func,0);
+            a(i)=exp(-b*c);
+            if i>1
+                %Compute integral with numerical quadrature
+                I = integral(f, a(i-1), a(i));
+                %Compute Q(n) sum
+                Q(n/100) = Q(n/100) + I; 
+            end
+        end
+        if (n/100)>3
+            %Compute Aitken's triangle square method
+            q((n/100)-3) = Q((n/100)-3) - (( Q((n/100)-2) - Q((n/100)-1) ).^2 / ( Q(n/100) - 2.*Q((n/100)-1) + Q((n/100)-2) ));
+        end
+    end
 end
