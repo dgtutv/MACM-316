3fa84559d9ee547d16cc743af2e81d1880836b53
Daniel <dgt1@sfu.ca>
2022-11-03 22:12:29 -0700

    Commit before optimization change

--- Assignment 6/CA6_code.m
+++ Assignment 6/CA6_code.m
@@ -1,16 +1,15 @@
-n = 5;
-for i=1:n
-    b=fzero(@(x) x*exp(x)-i*pi,0);
-    a(i)=exp(-b);
-end
-Q = zeros();
-for i=1:length(a)
-    %Compute integral woth numerical quadrature
-    I = integral(f(x), a(i), x(i+1));
-    %Compute Q(n) sum
-    Q(n) = Q(n) + I;
-end
-
-function y = f(x)
-    y = (x^-1)*sin((x^-1)*log(x));
+Q = [];
+f = @(x) (x.^-1).*sin((x.^-1).*log(x));
+for n=100:100:1000
+    for i=1:n
+        b=fzero(@(x) x*exp(x)-i*pi,0);
+        a(i)=exp(-b);
+    end
+    Q(n/100) = 0;
+    for i=1:(n-1)
+        %Compute integral with numerical quadrature
+        I = integral(f, a(i), a(i+1));
+        %Compute Q(n) sum
+        Q(n/100) = Q(n/100) + I;
+    end
 end
