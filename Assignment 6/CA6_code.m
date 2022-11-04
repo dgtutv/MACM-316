c8bc22844b4401b2ae9791bfdc55ed913568e822
Daniel <dgt1@sfu.ca>
2022-11-03 22:19:21 -0700

    Inital commit of summation segment

--- Assignment 6/CA6_code.m
+++ Assignment 6/CA6_code.m
@@ -1,15 +1,18 @@
 Q = [];
 f = @(x) (x.^-1).*sin((x.^-1).*log(x));
 for n=100:100:1000
+    Q(n/100) = 0;
     for i=1:n
         b=fzero(@(x) x*exp(x)-i*pi,0);
         a(i)=exp(-b);
-    end
-    Q(n/100) = 0;
-    for i=1:(n-1)
-        %Compute integral with numerical quadrature
-        I = integral(f, a(i), a(i+1));
-        %Compute Q(n) sum
-        Q(n/100) = Q(n/100) + I;
+        if i~=1
+            %Compute integral with numerical quadrature
+            I = integral(f, a(i-1), a(i));
+            %Compute Q(n) sum
+            Q(n/100) = Q(n/100) + I; 
+        end
     end
 end
+format longg
+%How many digits of I can you accurately compute?
+disp(Q)
