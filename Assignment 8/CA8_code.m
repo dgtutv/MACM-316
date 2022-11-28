3fb768a8c1353d9b56dbb411f59f0060b5dfade9
Daniel <dgt1@sfu.ca>
2022-11-27 22:44:12 -0800

    Issue with vectors

--- Assignment 8/CA8_code.m
+++ Assignment 8/CA8_code.m
@@ -1,14 +1,27 @@
 %------------------------------Part 1-------------------------------------%
-e = 0.6;
-h = 0.0005;
-N = 200/h;
-%Initial values
-part1 = Euler(p, -1/(q(1).^2 +q(2).^2).^(3/2) * q, [1-0.6; 0], [0, sqrt(((1+0.6)/(1-0.6)))]);
 
-%Eulers method
-function output = Euler(dq, dp, q, p, h, N)
+%Initial values & Solve
+h = 0.0005;
+q = [1-0.6; 0];
+p = [0, sqrt(((1+0.6)/(1-0.6)))];
+dq = @(t, p, q) p;
+dp = @(t, p, q) -1/(q(1).^2 +q(2).^2).^(3/2) * q;
+[Q1, P1] = Euler(dq, dp, q, p, 0.0005, 0, 200);   
+
+%use their method for X, use wq, and wp tho
+
+%Plot in q1-q2 plane
+plot(Q1(1), Q1(2));
+
+%---------------------------Eulers Method---------------------------------%
+function [Q,P] = Euler(dq, dp, q, p, h, a, b)
+    N = (b-a)/h;
+    t = [a];
+    Q = [q];
+    P = [p];
     for i = 2:N
-        q(i) = q + dq * h;
-        p (i) = p + dp *h;
+        disp(p + dp(t,p,q) *h);
+        Q(i, :) = q + dq(t,p,q) * h;
+        P (i, :) = p + dp(t,p,q) *h;
     end
 end
